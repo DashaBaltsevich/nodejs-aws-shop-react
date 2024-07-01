f2f111d17e11142031e609c58107e004523530d2
Darya Baltsevich <dasha.baltsevich111@gmail.com>
2024-07-01 23:22:36 +0200

    task-5 Import Service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
 	product: "https://fdnp1lb1hd.execute-api.eu-central-1.amazonaws.com/prod",
 	order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-	import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+	import: "https://ooir0bd08e.execute-api.eu-central-1.amazonaws.com/prod",
 	bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 	cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 }
